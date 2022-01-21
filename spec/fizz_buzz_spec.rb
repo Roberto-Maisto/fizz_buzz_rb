11cd99c171348404e57e8a99ee2761cd559a523c
Roberto <maisto_roberto@hotmail.it>
2022-01-21 10:50:54 +0100

    adds specs for fizz rule

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../src/fizz_buzz'
+
+RSpec.describe FizzBuzz do
+  
+subject { FizzBuzz.new }
+
+it "is exprected to return a number if no game conditions are met" do
+    # ACT
+    result = subject.check(1)
+    # ASSERT
+    expect(result).to eq 1
+end
+
+it "is expected to return 'fizz' if the number is divisable by 3" do
+    result = subject.check(3)
+    expect(result).to eq 'fizz'
+end
+end
+
+
+
